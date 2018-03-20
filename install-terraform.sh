a42ea63653a7f950151d69cd487058f53517de5c
Michał Krawczyk <krawczyk.michal91@gmail.com>
2018-03-20 19:19:09 +0100

    Remove unneeded parameter from terraform download link

--- install-terraform.sh
+++ install-terraform.sh
@@ -2,7 +2,7 @@
 
 which terraform > /dev/null && echo "Terraform is already installed." && exit 1
 
-wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip?_ga=2.6239859.1116590757.1517691694-146696288.1517691694 \
+wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip \
   -O /tmp/terraform.zip
 
 unzip /tmp/terraform.zip -d /usr/local/bin
