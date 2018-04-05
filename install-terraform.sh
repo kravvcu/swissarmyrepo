436b51ea0a3a87ea24cb5563613b7aa2e4d0506d
Michal Krawczyk <krawczyk.michal91@gmail.com>
2018-04-05 12:17:10 +0200

    Bump terraform install to 0.11.5

--- install-terraform.sh
+++ install-terraform.sh
@@ -2,8 +2,9 @@
 
 which terraform > /dev/null && echo "Terraform is already installed." && exit 1
 
-wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip \
+# TODO get latest version
+wget https://releases.hashicorp.com/terraform/0.11.5/terraform_0.11.5_linux_amd64.zip \
   -O /tmp/terraform.zip
 
 unzip /tmp/terraform.zip -d /usr/local/bin
-chmod +x /usr/local/bin/terraform && rm /tmp/terraform*
+chmod +x /usr/local/bin/terraform && rm -rf /tmp/terraform*
