96d07324350b8d4a7df258007171cce89bef9135
Michal Krawczyk <krawczyk.michal91@gmail.com>
2018-04-09 19:02:12 +0200

    Deduplicate provider configuration with symlinks

--- gcp-playground/terraform/envs/playground-stage/network/terraform.config.tf
+++ gcp-playground/terraform/envs/playground-stage/network/terraform.config.tf
@@ -1,9 +1,3 @@
-provider "google" {
-  project = "${var.project}"
-  region  = "${var.region}"
-  zone    = "${var.zone}"
-}
-
 terraform {
   backend "gcs" {
     bucket = "i-wonder-whether-buckets-can-have-longer-names"
